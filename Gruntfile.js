237f6277b8356f2458ef5bb91a4ca4a280262309
ahmed zaki <ah.zakii@hotmail.com>
2017-11-18 23:00:41 +0200

    commit project

---
+++ Gruntfile.js
@@ -0,0 +1,46 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+
+		/**
+		 * Sass
+		 */
+		sass: {
+		  dev: {
+		    options: {
+		      style: 'expanded',
+		      sourcemap: 'none',
+		    },
+		    files: {
+		      'css/main.css': 'scss/main.scss',
+              'css/rtl.css': 'scss/rtl.scss'
+		    }
+		  },
+        dist: {
+        options: {
+          style: 'compressed',
+          sourcemap: 'none',
+        },
+        files: {
+          'css/main-min.css': 'scss/main.scss',
+          'css/rtl-min.css': 'scss/rtl.scss'
+        }
+      },
+		},
+
+	  	/**
+	  	 * Watch
+	  	 */
+		watch: {
+			css: {
+				files: '**/*.scss',
+				tasks: ['sass']
+			}
+		},
+
+	});
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.registerTask('default',['watch']);
+}
